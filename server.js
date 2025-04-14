d95e0a6d463a446e1acf326af8cfe4f6b6dac0ba
Patrick <pherion@pm.me>
2025-04-14 10:06:38 +0200

    Third commit

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+import pg from 'pg';
+
+const app = express();
+app.use (express().json());
+app.use (cors());
+
+const port = 5858;
+
+// const pool = new pg.Pool({
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+}
+);
