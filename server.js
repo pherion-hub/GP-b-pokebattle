cd2013328e41334f1d41f58b5144717e541c990b
ramil_novruzov <ramil.novruzov@gmx.de>
2025-04-14 16:00:09 +0200

    added corsOptions

--- server.js
+++ server.js
@@ -1,30 +1,38 @@
-import express from 'express';
-import cors from 'cors';
-import UserScore from "./models/UserScore.js"
+import express from "express";
+import cors from "cors";
+import UserScore from "./models/UserScore.js";
 
 const app = express();
-app.use (express.json());
-app.use (cors());
-
 const port = 5858;
 
-app.get("/leaderboard", async (req, res)=> {
-    const leaderboard = await UserScore.findAll()
-    res.json({ message: "Wer gewinnt?", data: leaderboard})
-})
+app.use(express.json());
 
-app.post("/leaderboard", async (req, res)=> {
-    const {name, score} = req.body
-    console.log (req.body)
-    // if (!name || !score ) return res.status(400).json({ message: 'All fields are required!' });
-    const newentry = await UserScore.create({
-        name, score
-    })
-    res.json({ message: "Wer gewinnt?", data: newentry})
-})
+const corsOptions = {
+  origin: "http://localhost:5173", // Replace this with the URL of your React app
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  allowedHeaders: "Content-Type,Authorization", // Allowed headers
+  credentials: false, // If you use cookies set to true
+};
 
+app.use(cors(corsOptions));
 
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+app.get("/leaderboard", async (req, res) => {
+  const leaderboard = await UserScore.findAll();
+  res.json({ message: "Wer gewinnt?", data: leaderboard });
 });
 
+app.post("/leaderboard", async (req, res) => {
+  const { name, score } = req.body;
+  console.log(req.body);
+  if (!name || !score)
+    return res.status(400).json({ message: "All fields are required!" });
+  const newentry = await UserScore.create({
+    name,
+    score,
+  });
+  res.json({ message: "Wer gewinnt?", data: newentry });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
